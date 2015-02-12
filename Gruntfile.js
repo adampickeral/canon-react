8c384bce6ac15331138e697075530c7306d9aca6
adampickeral <adam.pickeral@rackspace.com>
2015-02-12 05:17:21 -0500

    Initial tooling setup

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+      options: {
+        process: function (content, srcpath) {
+          return grunt.template.process(content);
+        }
+      }
+    },
+
+    react: {
+      options: {
+        harmony: true
+      },
+      src: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['**/*.jsx'],
+            dest: 'transpiled',
+            ext: '.js'
+          }
+        ]
+      },
+      test: {
+        files: [
+          {
+            expand: true,
+            cwd: 'test',
+            src: ['**/*.jsx'],
+            dest: 'test-built',
+            ext: '.js'
+          }
+        ]
+      }
+    },
+
+    clean: {
+      transpiled: ['transpiled'],
+      test: ['test-built']
+    },
+
+    watch: {
+      all: {
+        files: [
+          'src/**/*.jsx',
+          'src/**/*.js',
+          'test/**/*.jsx',
+          'test/**/*.js'
+        ],
+        tasks: ['build'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    browserify: {
+      test: {
+        files: {
+          'test_bundle.js': ['test-built/**/*.js']
+        },
+        options: {
+          transform: ['envify'],
+          verbose: true
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'amd/<%= pkg.name %>.js',
+        dest: 'amd/<%= pkg.name %>.min.js'
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-react');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+  grunt.registerTask('build', [
+    'clean:test',
+    'react:src',
+    'react:test',
+    'copy',
+    'browserify:test',
+    'uglify:build',
+    'clean:transpiled'
+  ]);
+
+  grunt.registerTask('default', ['build']);
+
+};
